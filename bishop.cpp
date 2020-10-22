392c19840b3901566a7d34b412d49dee17b4b811
Ranquer <odinomar18@gmail.com>
2020-10-22 18:34:49 -0500

    bishop works ez

--- bishop.cpp
+++ bishop.cpp
@@ -1,5 +1,8 @@
 #include "bishop.h"
+#include "chessboard.h"
+#include <QDebug>
 
+extern ChessBoard *board;
 Bishop::Bishop(bool team, QGraphicsItem *parent) : ChessPiece(team,parent){
     setImage();
 }
@@ -12,5 +15,90 @@ void Bishop::setImage(){
 }
 
 void Bishop::move(){
+    location.clear();
+    int row = this->getCurrentBlock()->getRowLocation();
+    int column = this->getCurrentBlock()->getColumnLocation();
+    int i = 0;
+    int j = 0;
+    QString team = this->getCurrentBlock()->getChessPiece()->getSide();
 
+    //Move rigt-top
+    i = row - 1;
+    j = column + 1;
+    for (; i >= 0 && j <= 7; i--, j++) {
+        if (board->blocks[i][j]->hasPiece() == true) {
+            if (board->blocks[i][j]->getChessPiece()->getSide() != team) {
+                location.append(board->blocks[i][j]);
+                colorPossibleLocations(location.last());
+                break;
+            }
+            else {
+                break;
+            }
+        }
+        else {
+            location.append(board->blocks[i][j]);
+            colorPossibleLocations(location.last());
+        }
+    }
+
+    //Move lefth-top
+    i = row - 1;
+    j = column - 1;
+    for (; i >= 0 && j >= 0; i--, j-- ){
+        if (board->blocks[i][j]->hasPiece() == true) {
+            if (board->blocks[i][j]->getChessPiece()->getSide() != team) {
+                location.append(board->blocks[i][j]);
+                colorPossibleLocations(location.last());
+                break;
+            }
+            else {
+                break;
+            }
+        }
+        else {
+            location.append(board->blocks[i][j]);
+            colorPossibleLocations(location.last());
+        }
+    }
+
+    //Move left-down
+    i = row + 1;
+    j = column + 1;
+    for (; i <= 7 && j <= 7; i++, j++){
+        if (board->blocks[i][j]->hasPiece() == true) {
+            if (board->blocks[i][j]->getChessPiece()->getSide() != team) {
+                location.append(board->blocks[i][j]);
+                colorPossibleLocations(location.last());
+                break;
+            }
+            else {
+                break;
+            }
+        }
+        else {
+            location.append(board->blocks[i][j]);
+            colorPossibleLocations(location.last());
+        }
+    }
+
+    //Move rigth-down
+    i = row + 1;
+    j = column -1;
+    for (; i <=7 && j >= 0; i++ , j--){
+        if (board->blocks[i][j]->hasPiece() == true) {
+            if (board->blocks[i][j]->getChessPiece()->getSide() != team) {
+                location.append(board->blocks[i][j]);
+                colorPossibleLocations(location.last());
+                break;
+            }
+            else {
+                break;
+            }
+        }
+        else {
+            location.append(board->blocks[i][j]);
+            colorPossibleLocations(location.last());
+        }
+    }
 }

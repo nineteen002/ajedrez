b7eff58fa961d7afc7638cd50f2f926a7c496f8a
Ranquer <odinomar18@gmail.com>
2020-11-12 20:02:26 -0600

    Not changes

--- king.cpp
+++ king.cpp
@@ -23,22 +23,6 @@ void King::move(){
     int j = 0;
     QString team = this->getCurrentBlock()->getChessPiece()->getSide();
 
-//    //Castling
-//    i = row;
-//    j = column;
-//    if  (this->isFirstMove == true){
-//        //Short castling
-//        if (board->blocks[i][j+1]->hasPiece() == false && board->blocks[i][j+2]->hasPiece() == false && board->blocks[i][j+3]->getChessPiece()->isFirstMove == true){
-//            location.append(board->blocks[i][j+2]);
-//            colorPossibleLocations(location.last());
-//        }
-//        //Long castling
-//        if (board->blocks[i][j-1]->hasPiece() == false && board->blocks[i][j-2]->hasPiece() == false && board->blocks[i][j-3]->hasPiece() == false && board->blocks[i][j-4]->getChessPiece()->isFirstMove == true){
-//            location.append(board->blocks[i][j-2]);
-//            colorPossibleLocations(location.last());
-//        }
-//    }
-
     //Top
     i = row - 1;
     j = column;

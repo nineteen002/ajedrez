171a6a0245070479e96a5a07261ba7bc8ff383cd
Ranquer <odinomar18@gmail.com>
2020-11-16 12:35:09 -0600

    Ajedrez login

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
 #include <QApplication>
 #include "chessboard.h"
 #include <QDebug>
+#include "mainwindow.h"
 
 ChessBoard* board;
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    board = new ChessBoard();
-    qDebug() << "1" <<board;
-    board->show();
-    board->start();
+//    board = new ChessBoard();
+//    qDebug() << "1" <<board;
+//    board->show();
+//    board->start();
+    MainWindow w;
+    w.show();
     return a.exec();
-    //MainWindow w;
-    //w.show();
+
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,7 +3,6 @@
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 }
